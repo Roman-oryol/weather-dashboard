bfe4dacaf47d6ddb8fd46bd64b185af4242e687e
Roman Oryol <roman.oryol83@gmail.com>
2026-08-11 12:25:54 +0300

    feat: добавить компоненты карты и легенды для отображения погодных данных

---
+++ src/components/Map.tsx
@@ -0,0 +1,70 @@
+import { MapContainer, Marker, TileLayer, useMap } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import { MaptilerLayer } from "@maptiler/leaflet-maptilersdk";
+import type { Coords } from "../types";
+import { useEffect } from "react";
+
+const API_KEY = import.meta.env.VITE_API_KEY;
+
+type Props = {
+  coords: Coords;
+  onMapClick: (lat: number, lon: number) => void;
+  mapType: string;
+};
+
+export default function Map({ coords, onMapClick, mapType }: Props) {
+  const { lat, lon } = coords;
+
+  return (
+    <MapContainer
+      center={[lat, lon]}
+      zoom={5}
+      style={{ width: "auto", height: "500px" }}
+    >
+      <MapClick onMapClick={onMapClick} coords={coords} />
+      <MapTileLayer />
+      <TileLayer
+        opacity={0.7}
+        url={`https://tile.openweathermap.org/map/${mapType}/{z}/{x}/{y}.png?appid=${API_KEY}`}
+      />
+      <Marker position={[lat, lon]} />
+    </MapContainer>
+  );
+}
+
+function MapClick({
+  onMapClick,
+  coords,
+}: {
+  onMapClick: (lat: number, lon: number) => void;
+  coords: Coords;
+}) {
+  const map = useMap();
+  map.panTo([coords.lat, coords.lon]);
+
+  map.on("click", (e) => {
+    const { lat, lng } = e.latlng;
+    map.panTo([lat, lng]);
+    onMapClick(lat, lng);
+  });
+
+  return null;
+}
+
+function MapTileLayer() {
+  const map = useMap();
+
+  useEffect(() => {
+    const tileLayer = new MaptilerLayer({
+      style: "basic-dark",
+      apiKey: "4vNmbXcuyV3GqjV1NtOE",
+    });
+    tileLayer.addTo(map);
+
+    return () => {
+      map.removeLayer(tileLayer);
+    };
+  }, [map]);
+
+  return null;
+}
