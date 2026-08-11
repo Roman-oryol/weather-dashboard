a9343002ac5101013fa3ee79bcc15cef9e387e11
Roman Oryol <roman.oryol83@gmail.com>
2026-08-11 12:22:45 +0300

    feat: добавить функции для получения погоды и геокодирования

---
+++ src/api.ts
@@ -0,0 +1,22 @@
+import { GeocodeSchema } from "./schemas/geocodeSchema";
+import { weatherSchema } from "./schemas/weatherSchema";
+
+const API_KEY = import.meta.env.VITE_API_KEY;
+
+export async function getWeather({ lat, lon }: { lat: number; lon: number }) {
+  const res = await fetch(
+    `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=minutely,alerts&lang=ru&appid=${API_KEY}`,
+  );
+  const data = await res.json();
+
+  return weatherSchema.parse(data);
+}
+
+export async function getGeocode(location: string) {
+  const res = await fetch(
+    `http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=${API_KEY}`,
+  );
+  const data = await res.json();
+
+  return GeocodeSchema.parse(data);
+}
